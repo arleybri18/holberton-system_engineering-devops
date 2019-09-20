11ed36088bbebe57aa024bce75fd4cd7a25e215c
arleybri18 <arleybri18@gmail.com>
2019-09-20 00:17:37 +0000

    [Fetaure] list a top ten of reddit

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+import requests
+from sys import argv
+
+
+def top_ten(subreddit):
+    """ get a top ten list reddits """
+    headers = {'User-Agent': 'Yony'}
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    res = requests.get(url, headers=headers, allow_redirects=False)
+    if res.status_code == requests.codes.ok:
+        cont = 0
+        res_json = res.json()
+        for subr in res_json['data']['children']:
+            if cont < 10:
+                print(subr['data']['title'])
+                cont += 1
+    else:
+        print('None')
