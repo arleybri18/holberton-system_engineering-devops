9a4b4d519bc5e4f934ccc55584aa6abbacf06bf3
arleybri18 <arleybri18@gmail.com>
2019-10-10 22:32:41 +0000

    fix limit open files

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,9 +1,9 @@
 # modify limit open files
-exec {'modify file':
-command =>  "sed -i 's/.holberton hard nofile 5/holberton hard nofile 4000/g' /etc/security/limits.conf",
+exec {'modify file hard':
+command =>  "sed -i 's/holberton hard nofile 5/holberton hard nofile 4000/g' /etc/security/limits.conf",
 path    => '/bin/',
 }
--> exec {'modify file':
-command =>  "sed -i 's/.holberton soft nofile 4/holberton soft nofile 4000/g' /etc/security/limits.conf",
-path    =>  '/usr/bin/',
+-> exec {'modify file soft':
+command =>  "sed -i 's/holberton soft nofile 4/holberton soft nofile 4000/g' /etc/security/limits.conf",
+path    =>  '/bin/',
 }
