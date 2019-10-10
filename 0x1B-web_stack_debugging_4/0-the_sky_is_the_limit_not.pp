b2b64e36a90c58ee4c9b369cc96b80214a1083e3
arleybri18 <arleybri18@gmail.com>
2019-10-10 21:46:55 +0000

    Fixed limit open files

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,8 +2,8 @@
 exec {'modify file':
 command => "sed -i 's/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/g'  /etc/default/nginx",
 path    => '/bin/',
-}->
-exec {'restart service':
-command => "sudo service nginx restart",
-path => '/usr/bin/',
+}
+-> exec {'restart service':
+command => 'sudo service nginx restart',
+path    => '/usr/bin/',
 }
